0448c7a584e7c4ddc0c5fa6340ec05e3b2e61858
Konsti <pfafenrod@gmail.com>
2022-03-15 16:38:20 +0100

    initial commit for the vue -modal component

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <h1>My Vue App</h1>
+  <p>
+    Welcome...
+  </p>
+  <button @click="toggleModal">Show normal modal</button>
+  <button @click="toggleModal2">Show sale modal</button>
+  <teleport to=".modals" v-if="showModal">
+    <Modal @close="toggleModal">
+      <h2>{{ modalHeader }}</h2>
+      <p>{{ modalText }}</p>
+      <template v-slot:links>
+        <a href="#">more info</a>
+        <a href="#">sing in</a>
+      </template>
+    </Modal>
+  </teleport>
+  <teleport to=".modals" v-if="showModal2">
+    <Modal theme="sale" @close="toggleModal2">
+      <h2>{{ modalHeader }}</h2>
+      <p>{{ modalText }}</p>
+      <template v-slot:links>
+        <a href="#">more info</a>
+        <a href="#">sing in</a>
+      </template>
+    </Modal>
+  </teleport>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data(){
+    return {
+      modalHeader: "This is a header",
+      modalText: "This is my Modal Text",
+      modalClass: "gray rounded",
+      showModal: false,
+      showModal2: false
+    }
+  },
+  methods:{
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModal2(){
+      this.showModal2 = !this.showModal2
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['theme'],
+        methods:{
+            closeModal(){
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+
+<style>
+    .backdrop{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+    }
+    .modal{
+        background: #fff;
+        width: 400px;
+        border-radius: 10px;
+        height: 500px;
+        height: max-content;
+        padding: 20px;
+        max-height: 80vh;
+        max-width: 80vw;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        box-shadow: 0 0 15px -5px black;
+        margin: auto;
+    }
+    .modal.sale{
+        background: crimson;
+        color: #fff;
+    }
+    .modal h2{
+        font-size: 1.5em;
+    }
+    .modal .actions a{
+        margin: 0 10px;
+        font-size: 12px;
+    }
+
+</style>
